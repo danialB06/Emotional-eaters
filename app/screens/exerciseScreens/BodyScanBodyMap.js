8937e30d98f382d843ba777f1494442a61508418
Makanmi <cant.use.outlook@gmail.com>
2022-05-15 04:03:23 +0200

    Bodymap Colour Toggle

--- app/screens/exerciseScreens/BodyScanBodyMap.js
+++ app/screens/exerciseScreens/BodyScanBodyMap.js
@@ -1,31 +1,57 @@
-import { View, Image, StyleSheet, Text } from "react-native";
-import ImageMapper from 'react-img-mapper';
+import { View, Image, StyleSheet, Text, Pressable } from "react-native";
+import { useState, useEffect } from 'react';
 
 export default function BodyScanBodyMap({ navigation }) {
-  const URL = "../../assets/bodyscan/body.png";
-  const MAP = {
-    name: 'my-map',
-    areas: '',
+
+  //Bodypart evaluation scores
+  const [evalForehead, setEvalForehead] = useState(1);
+  const [evalEyes, setEvalEyes] = useState(1);
+  const [evalJaw, setEvalJaw] = useState(1);
+  const [evalShoulders, setevalShoulders] = useState(1);
+
+  if(evalForehead == 1){
+    var foreheadImg = require("../../assets/bodyscan/headForehead.png");
+  }else{
+    var foreheadImg = require("../../assets/bodyscan/headForeheadB.png");
   }
+
+  if(evalEyes == 1){
+    var eyesImg = require("../../assets/bodyscan/headEyes.png");
+  }else{
+    var eyesImg = require("../../assets/bodyscan/headEyesB.png");
+  }
+
+  if(evalJaw == 1){
+    var jawImg = require("../../assets/bodyscan/headJaw.png");
+  }else{
+    var jawImg = require("../../assets/bodyscan/headJawB.png");
+  }
+
+  if(evalShoulders == 1){
+    var shoulderImg = require("../../assets/bodyscan/bodyShoulders.png");
+  }else{
+    var shoulderImg = require("../../assets/bodyscan/bodyShouldersB.png");
+  }
+
   return(
   <View style={styles.container}>
       <Text style={styles.pageTitle}>Bodymap</Text>
         <Image //Head
           style={styles.bodyImage}
-          source={require("../../assets/bodyscan/headForehead.png")}
+          source={foreheadImg}
         />
         <Image
           style={styles.bodyImage}
-          source={require("../../assets/bodyscan/headEyes.png")}
+          source={eyesImg}
         />
         <Image
           style={styles.bodyImage}
-          source={require("../../assets/bodyscan/headJaw.png")}
+          source={jawImg}
         />
 
         <Image //Body
           style={styles.bodyImage}
-          source={require("../../assets/bodyscan/bodyShoulders.png")}
+          source={shoulderImg}
         />
         <Image
           style={styles.bodyImage}
@@ -67,8 +93,65 @@ export default function BodyScanBodyMap({ navigation }) {
         />
 
         <Image //Body outline - needs to be drawn last!
-          
+          style={styles.bodyImage}
+          source={require("../../assets/bodyscan/body.png")}
         />
+
+        <Pressable 
+          style={bodyParts.bodyForehead}
+          onPress={()=>setEvalForehead(evalForehead * -1)}
+        />
+        <Pressable 
+          style={bodyParts.bodyEyes}
+          onPress={()=>setEvalEyes(evalEyes * -1)}
+        />
+        <Pressable 
+          style={bodyParts.bodyJaw}
+          onPress={()=>setEvalJaw(evalJaw * -1)}
+        />
+
+        <Pressable 
+          style={bodyParts.bodyShoulders}
+          onPress={()=>setevalShoulders(evalShoulders * -1)}
+        />
+        <Pressable 
+          style={bodyParts.bodyChest}
+        />
+        <Pressable 
+          style={bodyParts.bodyGut}
+        />
+        <Pressable 
+          style={bodyParts.bodyCrotch}
+        />
+
+        <Pressable 
+          style={bodyParts.bodyUpperlegs}
+        />
+        <Pressable 
+          style={bodyParts.bodyLowerLegs}
+        />
+        <Pressable 
+          style={bodyParts.bodyFeet}
+        />
+
+        <Pressable 
+          style={bodyParts.bodyUpperArmLeft}
+        />
+        <Pressable 
+          style={bodyParts.bodyUpperArmRight}
+        />
+        <Pressable 
+          style={bodyParts.bodyLowerArmLeft}
+        />
+        <Pressable 
+          style={bodyParts.bodyLowerArmRight}
+        />    
+        <Pressable 
+          style={bodyParts.bodyHandLeft}
+        />
+        <Pressable 
+          style={bodyParts.bodyHandRight}
+        />                                 
     </View>
   );
 }
@@ -97,7 +180,116 @@ const styles = StyleSheet.create({
   },
   bodyImage:{
     padding: 40,
-    top: '15%',
+    top: 80,
     position: "absolute",
+  },
+});
+
+const bodyParts = StyleSheet.create({
+  bodyPartContainer:{
+    flexDirection: 'row',
+  },
+  bodyForehead:{
+    position: "absolute",
+    top: 80,
+    width: 129,
+    height: 44,
+  },
+  bodyEyes:{
+    position: "absolute",
+    top: 124,
+    width: 126,
+    height: 34,
+  },
+  bodyJaw:{
+    position: "absolute",
+    top: 158,
+    width: 126,
+    height: 34,
+  },
+  bodyShoulders:{
+    position: "absolute",
+    top: 190,
+    width: 184,
+    height: 40,
+  },
+  bodyChest:{
+    position: "absolute",
+    top: 230,
+    width: 94,
+    height: 56,
+  }
+  ,
+  bodyUpperArmLeft:{
+    position: "absolute",
+    top: 230,
+    left: 96,
+    width: 60,
+    height: 50,
+  },
+  bodyUpperArmRight:{
+    position: "absolute",
+    top: 230,
+    right: 96,
+    width: 60,
+    height: 50,
+  },
+  bodyGut:{
+    position: "absolute",
+    top: 286,
+    width: 98,
+    height: 44,
+  },
+  bodyCrotch:{
+    position: "absolute",
+    top: 328,
+    width: 128,
+    height: 48,
+  },
+  bodyUpperlegs:{
+    position: "absolute",
+    top: 376,
+    width: 140,
+    height: 84,
+  },
+  bodyLowerLegs:{
+    position: "absolute",
+    top: 460,
+    width: 172,
+    height: 96,
+  },
+  bodyFeet:{
+    position: "absolute",
+    top: 552,
+    width: 212,
+    height: 54,
+  },
+  bodyLowerArmLeft:{
+    position: "absolute",
+    top: 280,
+    left: 80,
+    width: 64,
+    height: 60,
+  },
+  bodyHandLeft:{
+    position: "absolute",
+    top: 338,
+    left: 62,
+    width: 72,
+    height: 78,   
+  },
+  bodyLowerArmRight:{
+    position: "absolute",
+    top: 280,
+    right: 80,
+    width: 64,
+    height: 60,
+  },
+  bodyHandRight:{
+    position: "absolute",
+    top: 338,
+    right: 62,
+    width: 72,
+    height: 78, 
   },
 });
