a677f523e67f57828fd920cb07146e51c3c18279
Makanmi <cant.use.outlook@gmail.com>
2022-06-08 17:12:17 +0200

    Alarms Screen relocated

--- app/screens/homeScreens/AlarmsScreen.js
+++ app/screens/homeScreens/AlarmsScreen.js
@@ -1,14 +1,20 @@
-import { View, StyleSheet, Text, Image, Button } from "react-native";
+import { View, StyleSheet,Image, Text, Button } from "react-native";
+import { Pressable } from "react-native";
 
 export default function AlarmsScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Image style={styles.bodyImage} source={require("../../assets/bodyscan/body.png")}/>
-
-      <Text>Alarms</Text>
+      <Text style={styles.title}>Notifications</Text>
+      <Pressable onPress={() => navigation.navigate("NewAlarmScreen")}
+      >
+        <Image style={styles.addImage} source={require("../../assets/homescreen/goalsscreen/add.png")}/>
+      </Pressable>
     </View>
   );
+  
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +22,16 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 30,
+  },
+  addImage: {
+    width: 35,
+    height: 35,
+    marginHorizontal: 20,
+    marginTop: 20,
+    alignSelf: "flex-end",
   },
 });
