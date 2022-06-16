92ba81b61d6c8704691efe7f8d31760e287d43b1
Simona Salatkaite <simona.salatkaite@gmail.com>
2022-06-16 17:20:19 +0200

    adding firebase

--- app/api/Firebase.js
+++ app/api/Firebase.js
@@ -1,13 +1,20 @@
 import * as firebase from "firebase";
 
 // Your web app's Firebase configuration
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDKjvmVH_3IuJhUW2XHOcmNVMQti7hToPE",
+  authDomain: "emotional-eaters.firebaseapp.com",
+  projectId: "emotional-eaters",
+  storageBucket: "emotional-eaters.appspot.com",
+  messagingSenderId: "953761521525",
+  appId: "1:953761521525:web:64e7679c5ec7d1ec9b3d3c",
+};
 
 let app;
-if(firebase.apps.length === 0){
-    app = firebase.initializeApp(firebaseConfig);
-}else{
-    app = firebase.app();
+if (firebase.apps.length === 0) {
+  app = firebase.initializeApp(firebaseConfig);
+} else {
+  app = firebase.app();
 }
 
 const auth = firebase.auth();
