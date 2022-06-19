a7e0e95659b31236b770ed4de61aebbf40e684a4
Simona Salatkaite <simona.salatkaite@gmail.com>
2022-06-19 21:16:12 +0200

    pushing Alex's code

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -2,8 +2,8 @@ import { View, StyleSheet, Text, Button } from "react-native";
 import { useEffect, useState } from "react";
 import * as SecureStore from "expo-secure-store";
 import { useLogin, fetchUser } from "../context/LoginProvider";
-
-
+import { Image } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
 export default function ProfileScreen({ navigation }) {
   const [username, setUsername] = useState("");
@@ -14,22 +14,21 @@ export default function ProfileScreen({ navigation }) {
 
   //retrives data from secureStore
   async function loadUserData() {
-    let tempUsername  = await SecureStore.getItemAsync("username");
-    if(tempUsername){
+    let tempUsername = await SecureStore.getItemAsync("username");
+    if (tempUsername) {
       setUsername(tempUsername);
     }
     let tempEmail = await SecureStore.getItemAsync("email");
     if (tempEmail) {
       setEmail(tempEmail);
-    }else{
-
+    } else {
     }
     let tempDateOfBrith = await SecureStore.getItemAsync("dateOfBirth");
     if (tempDateOfBrith) {
       setDateOfBirth(tempDateOfBrith);
     }
   }
-  
+
   //deletes the userdata from divces secure storage
   async function signout() {
     SecureStore.deleteItemAsync("username");
@@ -39,26 +38,103 @@ export default function ProfileScreen({ navigation }) {
     setIsLoggedIn(false);
   }
 
-   useEffect(() => {
+  useEffect(() => {
     loadUserData();
-  })
+  });
 
   return (
     <View style={styles.container}>
-      <Text>ProfileScreen</Text>
-      <Text>Username: {username}</Text>
-      <Text>Email:  {email}</Text>
-      <Text>dateOfBirth:  {dateOfBirth}</Text>
-      <Button onPress={() => signout()}  title="signout"/>
+      <View style={styles.circle}>
+        <FontAwesome
+          name="user"
+          size={150}
+          color="black"
+          style={{ alignSelf: "center" }}
+        />
+      </View>
+
+      <View style={styles.usercircle}>
+        <Text> Basic User </Text>
+      </View>
+
+      <View style={styles.joined}>
+        <Text> Joined 13-04-2022 </Text>
+      </View>
+      <View style={styles.profileInfo}>
+        {/* <Text style={styles.test}>John Doe</Text> */}
+        <Text style={styles.username}>Username: {username} </Text>
+        <Text style={styles.email}>Email: {email} </Text>
+        <Text style={styles.dateOfBirth}>Date of birth: {dateOfBirth}</Text>
+      </View>
+      <View style={{ padding: 80 }}>
+        <Button onPress={() => signout()} title="Sign Out" />
+      </View>
     </View>
   );
 }
-                      
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "white",
+    backgroundColor: "#B8D5EC",
     alignItems: "center",
+  },
+  profileInfo: {
+    marginHorizontal: 15,
+    alignContent: "flex-start",
+  },
+  test: {
+    color: "black",
+    fontSize: 30,
+    paddingTop: 10,
+  },
+  circle: {
+    backgroundColor: "#FFF",
+    borderRadius: 1000,
+    borderWidth: 1,
+    borderColor: "#000000",
+    width: 200,
+    marginTop: 40,
+    height: 200,
+    alignContent: "center",
     justifyContent: "center",
+  },
+  usercircle: {
+    borderRadius: 50,
+    backgroundColor: "white",
+    borderColor: "#000000",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+    position: "absolute",
+    width: 99,
+    height: 22,
+    left: 145,
+    top: 220,
+  },
+
+  joined: {
+    width: 165,
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  username: {
+    color: "black",
+    fontSize: 20,
+    paddingTop: 10,
+  },
+  email: {
+    color: "black",
+    fontSize: 20,
+    paddingTop: 10,
+  },
+
+  dateOfBirth: {
+    color: "black",
+    fontSize: 20,
+    paddingTop: 10,
+    paddingRight: 120,
   },
 });
